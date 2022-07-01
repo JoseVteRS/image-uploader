4d981207a9d987855b2ea79c4570dece9103d8c4
Jose Ripoll <jose.ripoll@acrismatic.es>
2022-07-01 08:55:59 +0200

    fix/imports

--- backend/src/infrastructure/repositories/user.repositories.js
+++ backend/src/infrastructure/repositories/user.repositories.js
@@ -1,30 +1,36 @@
-import { UserModel } from "../../domain/model/user-model.js";
-import { UserSchema } from "../schemas/user.schema.js";
+import { UserModel } from '../../domain/model/user-model.js';
+import { UserSchema } from '../schemas/user.schema.js';
 
 /**
  * User MongoDB repository implementation
  */
 export class UserRepository {
-
     /**
      * Transforms a database user into a domain user
      * @param {*} persistanceUser Database user
      * @returns Domain user
      */
-    static toDomain(persistanceUser) {
-        const { _id, email, name, password, profilePicture, images } = persistanceUser;
+    toDomain(persistanceUser) {
+        const { _id, email, name, password, profilePicture, images } =
+            persistanceUser;
 
-        return new UserModel(_id, name, email, password, profilePicture, images);
+        return new UserModel(
+            _id,
+            name,
+            email,
+            password,
+            profilePicture,
+            images
+        );
     }
-
-
     /**
      * Transforms a domain user into a database user
      * @param {UserModel} domainUser Domain user
      * @returns Database user
      */
-    static toPersistance(domainUser) {
-        const { id, name, email, password, profilePicture, images } = domainUser;
+    toPersistance(domainUser) {
+        const { id, name, email, password, profilePicture, images } =
+            domainUser;
 
         return { _id: id, email, name, password, profilePicture, images };
     }
@@ -34,13 +40,10 @@ export class UserRepository {
      * @param {String} id User id
      * @return Domain user
      */
-    static async findById(id) {
+    async findById(id) {
         const userFound = await UserSchema.findById(id).exec();
-
         if (!userFound) return null;
-
-        return UserRepository.toDomain(userFound);
-
+        return this.toDomain(userFound);
     }
 
     /**
@@ -48,23 +51,18 @@ export class UserRepository {
      * @param {String} email User email
      * @return Domain user
      */
-    static async findByEmail(email) {
+    async findByEmail(email) {
         const userFound = await UserSchema.findOne({ email }).exec();
-
         if (!userFound) return null;
-
-        return UserRepository.toDomain(userFound);
+        return this.toDomain(userFound);
     }
 
     /**
      * @param {UserModel} domainUser Domain user
      */
-    static async create(domainUser) {
-        const persistanceUser = UserRepository.toPersistance(domainUser);
-
+    async create(domainUser) {
+        const persistanceUser = this.toPersistance(domainUser);
         const user = new UserSchema(persistanceUser);
-
         await user.save();
     }
-
 }
