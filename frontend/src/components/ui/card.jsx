0c94a2fd410b9dbfa2f39f81ce2ed8fe15bf3619
Jose Ripoll <jose.ripoll@acrismatic.es>
2022-07-05 16:51:40 +0200

    fix/ajust position

--- frontend/src/components/ui/card.jsx
+++ frontend/src/components/ui/card.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({ children }) => {
 	return (
 		<div className='w-12/12 md:w-1/2 lg:w-1/3 mb-5 md:mb-0'>
-			<div className='bg-white rounded-2xl shadow-xl p-8 sticky top-8 ' >{children}</div>
+			<div className='bg-white rounded-2xl shadow-xl p-8 md:sticky top-20' >{children}</div>
 		</div>
 	);
 };
